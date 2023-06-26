f7570113ba753aa1a18845d04491294b6aff52ca
JoãoSantos <jv29645@gmail.com>
2023-06-25 22:37:05 -0300

    frontend inital commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const router = require('./router')
 const PORT = process.env.PORT
 
 app.use(express.json())
+app.use(cors())
 app.use(router)
+
 module.exports = app
